adb5e43fb0cae282f1a7f0688e8e2c876993cb32
urieleon15 <angeleon605@gmail.com>
2023-09-14 17:49:13 -0600

    V2 Rectangulo

--- GRFiguras.cpp
+++ GRFiguras.cpp
@@ -1,12 +1,10 @@
 #include <stdio.h>
 int main() {
 	float fBase, fAltura, fArea;
-	printf("Calcular el Area de un Cuadrado \n");
+	printf("Calcular el Area de un Rectangulo \n");
 	printf("introduzca la cantidad para la Base: ");
 	scanf_s("%f", &fBase);
 	printf("Introduzca la cantidad para la altura: ");
 	scanf_s("%f", &fAltura);
 	fArea = fBase * fAltura;
-	printf("El area para el cuadrado es: %f", fArea);
-
-}
+	printf("El area para el rectangulo es: %f", fArea);
