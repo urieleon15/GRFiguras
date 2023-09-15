ee790caf56893416dc81913c48a9339c69f4a308
urieleon15 <angeleon605@gmail.com>
2023-09-14 18:00:38 -0600

    V3 Circulo

--- GRFiguras.cpp
+++ GRFiguras.cpp
@@ -1,10 +1,12 @@
 #include <stdio.h>
-int main() {
-	float fBase, fAltura, fArea;
-	printf("Calcular el Area de un Rectangulo \n");
-	printf("introduzca la cantidad para la Base: ");
-	scanf_s("%f", &fBase);
-	printf("Introduzca la cantidad para la altura: ");
-	scanf_s("%f", &fAltura);
-	fArea = fBase * fAltura;
+int main()
+{
+	float fArea, fRadio;
+	printf("Calcular el Area de un Circulo \n");
+	printf("introduzca la cantidad para el radio: ");
+	scanf_s("%f", &fRadio);
+	fArea = fRadio * fRadio * 3.1416;
 	printf("El area para el rectangulo es: %f", fArea);
+
+
+}
